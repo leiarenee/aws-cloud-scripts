e34e0b5ea043fbfb0b81bef7e1ea696918a5c57c
Leia Renee <leiarenee20@gmail.com>
2021-05-18 05:51:46 +0300

    degub 4

--- nuke/codebuild.sh
+++ nuke/codebuild.sh
@@ -5,7 +5,8 @@ set -e
 # https://www.1strategy.com/blog/2019/07/16/automated-clean-up-with-aws-nuke-in-multiple-accounts/
 
 aws --version
-
+aws rds describe-option-groups
+exit 0
 # Remove current temporary directory if it already exits
 rm -R -f temp
 
